ac15dd501845a53b0523603a7b38bfcd00d90d35
zhangyan <zhangyan@jingoal.com>
2017-09-08 17:20:15 +0800

    配置生产开发环境

--- src/index.js
+++ src/index.js
@@ -1,35 +1,18 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import { cube } from './math.js';
 
-// ReactDOM.render(
-//     <h1>Hello, world!</h1>,
-//     document.getElementById('root')
-// );
-
-import _ from 'lodash';
-import printMe from './print.js';
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
 
 function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
-
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack', '中文'], ' ');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
+    var element = document.createElement('pre');
+    
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
 
     return element;
 }
 
 document.body.appendChild(component());
-
-
-if (module.hot) {
-    module.hot.accept('./print.js', function() {
-        console.log('Acceptint the updated printMe module!');
-        printMe();
-    })
-}
